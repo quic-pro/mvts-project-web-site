96a7016765ceeea81ff159ad7f75d70a046a3af4
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-06 12:49:55 +0700

    Add height and delete one of meta

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import mvtsLogo from './img/Vector.png';
 
 function App() {
   return (
-    <div class="bg-mainBlack">
+    <div class="bg-mainBlack h-screen">
       <head>
 
       </head>
       <div class="bg-mainBlack">
         <div class="relative">
+        <center>
           <img src={robotImg} class="resize pt-8 px-6" alt="robot"/>
+        </center>
           <p class="text-5xl font-sans text-bigText">
             <center>
             Coming soon
@@ -28,28 +30,6 @@ function App() {
       </div>
     </div>
 
-    // <div className="App">
-    //     <header className="App-header"> </header>
-    //     <body className="App-body">
-    //         <div className="Div-top">
-    //             <img src={robotImg} className="Robot-Img" alt="robot" />
-    //             <p className="Main-text">
-    //                 Coming soon
-    //             {/* <div id="first_id_for_stud"></div> */}
-    //             </p>
-    //         </div>
-    //         <div className="Div-bot">
-    //             <img src={mvtsLogo} className="Mvts-Logo" alt="logo"/>
-    //             <p className="Text-bot">
-    //                 «MetaVerse Telecom Service» © 2022. All rights reserved.
-    //             </p>
-
-    //         </div>
-            
-    //     </body>
-       
-    //   {/* </header> */}
-    // </div>
   );
 }
 
