f1100ac27e5c1c04529ffbbbb921e7541d5f0750
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-06 17:41:08 +0700

    change bottom line

--- src/App.js
+++ src/App.js
@@ -4,25 +4,23 @@ import mvtsLogo from './img/Vector.png';
 
 function App() {
   return (
-    <div class="bg-mainBlack h-screen">
+    <div class="bg-mainBlack h-screen static">
       <head>
 
       </head>
-      <div class="bg-mainBlack h-screen">
-        <div class="static w-full grid grid-cols-2 ph:grid-cols-1 relative items-center">
-        {/* <center> */}
-          <img src={robotImg} class="resize static pt-8 px-6 object-scale-down ph:order-1 order-2" alt="robot"/>
-        {/* </center> */}
+      <div class="bg-mainBlack h-screen static">
+        <div class="static w-full grid grid-cols-2 ph:grid-cols-1 ph:static relative inset-y-1/4 items-center">
+          <img src={robotImg} class="resize ph:pt-8 px-6 object-scale-down ph:order-1 order-2" alt="robot"/>
           <div class="text-5xl font-Comfort text-bigText text-center ph:order-2 order-1">
             Coming soon
           </div>
         </div>
-        <div class="bg-gradient-to-b from-gradientBlack to-mainBlack ph:h-1/4 h-1/2 relative">
+        <div class="bg-gradient-to-b from-gradientBlack to-mainBlack ph:h-1/4 h-1/2 static bottom-0">
           <div class="pb-6 px-3 absolute bottom-0 w-full">
-            <img src={mvtsLogo} class="float-left resize px-1 h-2" alt="logo"/>
-            <p class="font-/Montserr text-botText/75 text-xxs absolute right-0 px-2">
+            <img src={mvtsLogo} class="float-left resize px-1 h-3" alt="logo"/>
+            <div class="font-Montserr text-botText/75 text-xxs absolute right-0 px-2">
               «MetaVerse Telecom Service» © 2022. All rights reserved.
-            </p>
+            </div>
           </div>
         </div>
       </div>
