7c9d00df64647d7bc91130eaef993bddf3cf1d42
Zmeesvetka <105983414+Zmeesvetka@users.noreply.github.com>
2022-06-07 11:25:01 +0700

    bottom

--- src/App.js
+++ src/App.js
@@ -8,19 +8,17 @@ function App() {
       <head>
 
       </head>
-      <div class="bg-mainBlack h-screen static">
-        <div class="static w-full grid grid-cols-2 ph:grid-cols-1 ph:static relative inset-y-1/4 items-center">
+      <div class="bg-mainBlack ph:h-3/4 h-1/2 static">
+        <div class="grid grid-cols-2 ph:grid-cols-1 ph:static relative inset-y-1/4 items-center">
           <img src={robotImg} class="resize ph:pt-8 px-6 object-scale-down ph:order-1 order-2" alt="robot"/>
           <div class="text-5xl font-Comfort text-bigText text-center ph:order-2 order-1">
             Coming soon
           </div>
         </div>
-        <div class="bg-gradient-to-b from-gradientBlack to-mainBlack ph:h-1/4 h-1/2 static bottom-0">
-          <div class="pb-6 px-3 absolute bottom-0 w-full">
-            <img src={mvtsLogo} class="float-left resize px-1 h-3" alt="logo"/>
-            <div class="font-Montserr text-botText/75 text-xxs absolute right-0 px-2">
-              «MetaVerse Telecom Service» © 2022. All rights reserved.
-            </div>
+        <div class="grid grid-cols-2 content-end items-center p-2 bg-gradient-to-b from-gradientBlack to-mainBlack ph:h-1/4 h-1/2 static">
+          <img src={mvtsLogo} class="resize" alt="logo"/>
+          <div class="font-Montserr text-botText/75 text-xxs">
+            «MetaVerse Telecom Service» © 2022. All rights reserved.
           </div>
         </div>
       </div>
